e3374433ca2606740d3dabfa1576c1ff9370a0ed
guillo <you@example.com>
2016-10-07 17:19:49 +0200

    Removing homebrew

--- opsworks-surveysapi/recipes/deploy.rb
+++ opsworks-surveysapi/recipes/deploy.rb
@@ -3,7 +3,9 @@ instance = search('aws_opsworks_instance', 'self:true').first
 layer = search('aws_opsworks_layer', "layer_id:#{instance['layer_ids'].first}").first
 
 # user layer name as app
-app_data = search('hello_world', "name:#{layer['name']}").first  
+#app_data = search('aws_opsworks_app', "name:#{layer['name']}").first  
+app_data = search('aws_opsworks_app', "name:hello_world").first  
+
 fail 'could not find app' unless app_data
 
 # deploy the application
