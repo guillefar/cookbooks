553c2abdbd8aa5b90c0cda6c1328a86d67739973
guillo <you@example.com>
2016-10-08 19:53:42 +0200

    Changing app names

--- opsworks-surveysapi/recipes/setup.rb
+++ opsworks-surveysapi/recipes/setup.rb
@@ -1,6 +1,6 @@
 
 
-nodejs_app_setup 'nodejs-app' do  
+nodejs_app_setup 'hello_world' do  
   nodejs_version node['opsworks-surveysapi']['nodejs']['version']
   nodejs_checksum node['opsworks-surveysapi']['nodejs']['checksum']
 end  
