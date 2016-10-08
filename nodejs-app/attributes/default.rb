5ef764db6dd35d18c5eb55801a66588fdc71200e
guillo <you@example.com>
2016-10-08 21:21:07 +0200

    Adding checksum

--- nodejs-app/attributes/default.rb
+++ nodejs-app/attributes/default.rb
@@ -2,4 +2,4 @@ default['opsworks-surveysapi']['basedir'] = '/opt'
 default['opsworks-surveysapi']['run-cmd'] = 'npm start'  
 
 default['opsworks-surveysapi']['nodejs']['version'] = '6.5.0'  
-default['opsworks-surveysapi']['nodejs']['checksum'] = 'd6480ab5bcfd124f471c7b535e6c2211addfc4f8c0dfa3d8777452f766c3f6ab'
+default['opsworks-surveysapi']['nodejs']['checksum'] = '...'
--- opsworks-surveysapi/attributes/default.rb
+++ opsworks-surveysapi/attributes/default.rb
@@ -1,6 +1,6 @@
 #   setup attributes
 default['opsworks-surveysapi']['nodejs']['version'] = '6.5.0'  
-default['opsworks-surveysapi']['nodejs']['checksum'] = '...'
+default['opsworks-surveysapi']['nodejs']['checksum'] = '52ad6d5030be62ac6072d08e0ee8fa25e50c3295107e1ff34abd3d31b1d67db4'
 
 #   deploy attributes
 default['opsworks-surveysapi']['basedir'] = '/opt'  
