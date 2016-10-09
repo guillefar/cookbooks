accb1f1db5433e843d535fe8e0c886a8e43c3792
guillo <you@example.com>
2016-10-09 20:48:51 +0200

    Adding checksum

--- opsworks-surveysapi/attributes/default.rb
+++ opsworks-surveysapi/attributes/default.rb
@@ -1,6 +1,9 @@
 #   setup attributes
 default['opsworks-surveysapi']['nodejs']['version'] = '6.5.0'  
-default['opsworks-surveysapi']['nodejs']['checksum'] = '12d5b79b8b914c2439b6aa542b47f28c0d6dc9c5438eeda629f26021eb839dac'
+default['opsworks-surveysapi']['nodejs']['checksum'] = 'd7742558bb3331e41510d6e6f1f7b13c0527aecc00a63c3e05fcfd44427ff778'
+
+
+# 12d5b79b8b914c2439b6aa542b47f28c0d6dc9c5438eeda629f26021eb839dac
 
 
 #SHA512 node-v6.5.0-linux-x64.tar.gz 575638830e4ba11c5afba5c222934bc5e338e74df2f27ca09bad09014b4aa415
