08977d624241a55e43b19d2292cebc8016c32ce2
guillo <you@example.com>
2016-10-13 04:19:50 +0200

    Fixing service start

--- opsworks-surveysapi/recipes/deploy.rb
+++ opsworks-surveysapi/recipes/deploy.rb
@@ -1,10 +1,13 @@
 # get own instances + layer name
 instance = search('aws_opsworks_instance', 'self:true').first  
-layer = search('aws_opsworks_layer', "shortname:surveysapi").first
+layer = search('aws_opsworks_layer', "layer_id:#{instance['layer_ids'].first}").first
 
 # user layer name as app
 #app_data = search('aws_opsworks_app', "name:#{layer['name']}").first  
-app_data = search('aws_opsworks_app', "app_id:1915b21a-5396-47b8-8688-0980e5780d8e").first  
+
+app_data = search('aws_opsworks_app', "shortname:surveysapi").first  
+
+#app_data = search('aws_opsworks_app', "app_id:1915b21a-5396-47b8-8688-0980e5780d8e").first  
 
 fail 'could not find app' unless app_data
 
