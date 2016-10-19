c743ffcb8c8619c7348055cdf361beeb8d6fa19c
guillo <you@example.com>
2016-10-19 21:50:52 +0200

    Adding letsencrypt

--- opsworks-surveysapi/recipes/setup.rb
+++ opsworks-surveysapi/recipes/setup.rb
@@ -2,7 +2,7 @@
 nodejs_app_setup 'surveysapi' do  
   nodejs_version node['opsworks-surveysapi']['nodejs']['version']
   nodejs_checksum node['opsworks-surveysapi']['nodejs']['checksum']
-  nodejs_adminemail "admin@uderm.md"
+  adminemail "admin@uderm.md"
   subdomain node["opsworks"]["instance"]["hostname"]+".uderm.md"
 
 end  
