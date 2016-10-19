da324eb77cdb7c47d47f8919bd853ac54c339b8c
guillo <you@example.com>
2016-10-19 03:17:43 +0200

    Choosing the branch according to the instance

--- opsworks-surveysapi/recipes/deploy.rb
+++ opsworks-surveysapi/recipes/deploy.rb
@@ -9,7 +9,7 @@ app_data = search('aws_opsworks_app', "shortname:surveysapi").first
 
 
 
-revision_string = node['opsworks-surveysapi']['instance']['aws_instance_id'].to_s.spl­it(//).las­t(4).join
+revision_string = node['instance']['aws_instance_id'].to_s.spl­it(//).las­t(4).join
 revision = revision_string.­split(//).­first(3).j­oin
 
 #app_data = search('aws_opsworks_app', "app_id:1915b21a-5396-47b8-8688-0980e5780d8e").first  
