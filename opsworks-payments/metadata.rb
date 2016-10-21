5ec81d8809b879425f3ff2047048ac163bc2195c
guillo <you@example.com>
2016-10-21 20:26:03 +0200

    Adding new app cookbook

--- opsworks-payments/metadata.rb
+++ opsworks-payments/metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Installs and Configures node.js, deploys the app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/guillefar/PaymentsCookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/guillefar/PaymemtsCookbook/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/guillefar/PaymentsCookbook/issues' if respond_to?(:issues_url)
 version '0.0.1'
 
 depends 'yum-epel'
@@ -28,5 +28,5 @@ end
 
 
 recipe "opsworks-payments::deploy", "For deployment"
-recipe "opsworks-paymemts::setup", "For setup"
+recipe "opsworks-payments::setup", "For setup"
 #recipe 'ssh_known_hosts', "SSH hosts"
