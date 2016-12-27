7296b226d89db225b85d8ccc399e2784fe9dc785
guillo <you@example.com>
2016-12-27 23:20:36 +0100

    Changing port, adding NewRelic

--- newrelic/metadata.rb
+++ newrelic/metadata.rb
@@ -13,10 +13,8 @@ end
 source_url 'https://github.com/djoos-cookbooks/newrelic' if respond_to?(:source_url)
 issues_url 'https://github.com/djoos-cookbooks/newrelic/issues' if respond_to?(:issues_url)
 
-depends 'python'
 depends 'curl'
 depends 'apt'
-depends 'yum', '~> 3.0'
 
 recipe 'newrelic', 'Adds the New Relic repository, installs & configures the New Relic server monitor agent.'
 recipe 'newrelic::repository', 'Adds the New Relic repository.'
